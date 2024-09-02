17244640b63148b7180711edb388370c180b098f
CatalinaRpoS <catrestreposa@unal.edu.co>
2024-09-02 15:59:20 -0500

    feat: run workflow

--- data_ingestion.py
+++ data_ingestion.py
@@ -2,19 +2,19 @@ import pandas as pd
 import os
 
 def generate_csv_files(path):
-    df = {"phrase": [], "sentiment": []}
+    df = {'phrase': [], 'sentiment': []}
     
     for name, _, files in os.walk(path):
         for file in files:
-            if file.endswith(".txt"):
-                with open(os.path.join(name, file), "r") as f:
-                    df["phrase"].append(f.read())
-                    df["sentiment"].append(os.path.basename(name))
+            if file.endswith('.txt'):
+                with open(os.path.join(name, file), 'r') as f:
+                    df['phrase'].append(f.read())
+                    df['sentiment'].append(os.path.basename(name))
 
     df = pd.DataFrame(df) 
-    df.to_csv(f"{os.path.basename(path)}_dataset.csv", index=False)
+    df.to_csv(f'{os.path.basename(path)}_dataset.csv', index=False)
 
-folders = ["data/train", "data/test"]
+folders = ['data/train', 'data/test']
 
 for folder in folders:
     generate_csv_files(folder)
